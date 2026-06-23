a3160b5dcc482a4ce14843da0e7636dd69df7eb8
snaens <snaens@snaens.net>
2026-06-23 03:03:15 +0200

    we want a acceleration limiter, not a smoother, lol
    - rename all things to limiter
    - value clamp near limit is unnessecary, since a limiter is linear
    - add a readme!

---
+++ README.md
@@ -0,0 +1,27 @@
+# Twist limiter
+
+Clamps values on `twist_limiter/in` to the maximum m/s² defined in the parameter `acceleration_limit_mps2`,  
+and publishes the result to `twist_limiter/out`.  
+Also features a 2-second timeout, upon which it decelerates down to 0.
+
+## Use case
+Say you have a gamepad controller, and want to teleop a robot.  
+You push the controller forwards, the robot leaps into action, accelerates way too fast, and breaks something.  
+Fun!  
+
+This node clamps the maximum acceleration you can give a robot to a preset value.  
+Now you can teleop the robot much like videogame controls, and other nodes no longer need a soft-start.  
+Neato!
+
+## Parameters
+`acceleration_limit_mps2`, default 1.5: maximum acceleration to clamp values to  
+`publisher_hz` default 20: how many hertz to run the publisher at
+
+
+## Tips
+- remap the topics to something sensible: `ros2 run twist_limiter twist_limiter_node --ros-args -r twist_limiter/out:=/georg/cmd_vel`
+- reconfigure values: `ros2 run twist_limiter twist_limiter_node --ros-args -p publisher_hz:=42`
+
+---
+
+Happy driving!
--- src/main.cpp
+++ src/main.cpp
@@ -9,22 +9,21 @@ using namespace std::chrono_literals;
  * applies a PT1 regulator to it,
  * and publishes the result */
 
-class TwistSmoother : public rclcpp::Node {
+class TwistLimiter : public rclcpp::Node {
 
 public:
-    TwistSmoother() : Node("twist_smoother") {
+    TwistLimiter() : Node("twist_limiter") {
         this->declare_parameter("acceleration_limit_mps2", 1.5);
-        this->declare_parameter("cutoff_threshold", 0.05);
         this->declare_parameter("publisher_hz", 20);
 
         subscription_ = this->create_subscription<geometry_msgs::msg::Twist>(
-            "twist_smoother/in", 10, std::bind(&TwistSmoother::topic_callback, this, std::placeholders::_1));
-        publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("twist_smoother/out", 10);
+            "twist_limiter/in", 10, std::bind(&TwistLimiter::topic_callback, this, std::placeholders::_1));
+        publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("twist_limiter/out", 10);
         timer_ = this->create_wall_timer(
-            std::chrono::milliseconds(static_cast<int>(1000.0 / static_cast<double>(this->get_parameter("publisher_hz").as_int()))), std::bind(&TwistSmoother::timer_callback, this)
+            std::chrono::milliseconds(static_cast<int>(1000.0 / static_cast<double>(this->get_parameter("publisher_hz").as_int()))), std::bind(&TwistLimiter::timer_callback, this)
         );
 
-        RCLCPP_INFO(this->get_logger(), "twist smoother online!");
+        RCLCPP_INFO(this->get_logger(), "twist limiter online!");
     }
 
 private:
@@ -45,13 +44,9 @@ private:
         }
 
         const double max_velocity = this->get_parameter("acceleration_limit_mps2").as_double() / static_cast<double>(this->get_parameter("publisher_hz").as_int()); // m/s^2 / 1/s = m/s
-        const double cutoff_threshold = this->get_parameter("cutoff_threshold").as_double();
 
-        auto controller = [max_velocity, cutoff_threshold](double *out, const double in) -> void {
-            if (abs(in - (*out)) < cutoff_threshold) // hard cut off → we are close to limit (limit is reached in infinity time)
-                (*out) = (in < cutoff_threshold) ? 0.0 : in; // zero if approaching zero, else upper limit
-            else
-                (*out) += std::max(-max_velocity, std::min(max_velocity, in - (*out))); // limit max accel, min/max
+        auto controller = [max_velocity](double *out, const double in) -> void {
+            (*out) += std::max(-max_velocity, std::min(max_velocity, in - (*out))); // limit max accel, min/max
         };
         controller(&this->out_value.linear.x, this->in_value.linear.x);
         controller(&this->out_value.linear.y, this->in_value.linear.y);
@@ -77,7 +72,7 @@ private:
 
 int main(int argc, char * argv[]) {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<TwistSmoother>());
+    rclcpp::spin(std::make_shared<TwistLimiter>());
     rclcpp::shutdown();
     return 0;
 }
